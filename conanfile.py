98f5428451105537ae1741ad64616b3e599d2b41
Christian Fueller <christian.fueller@gmx.de>
2019-10-14 15:51:49 +0200

    Initial commit

---
+++ conanfile.py
@@ -0,0 +1,60 @@
+import os
+from conans import ConanFile, AutoToolsBuildEnvironment
+from conans.client import tools
+
+
+class GccConan(ConanFile):
+    name = "gcc"
+    version = "8.1.0"
+    license = "TDB"
+    url = "https://gcc.gnu.org/"
+    settings = "os_build", "arch_build"
+    build_policy = "missing"
+    description = "GCC installer. Useful as a build_requires."
+    no_copy_source = True
+
+    build_requires = "automake/1.14.1@bananamonster/build-tools"
+
+    @property
+    def source_url(self):
+        return f"https://ftp.gnu.org/gnu/gcc/gcc-{self.version}/gcc-{self.version}.tar.xz"
+
+    @property
+    def gcc_folder(self):
+        return f"gcc-{self.version}"
+
+    def source(self):
+        self.output.info(f"Downloading GCC sources from {self.source_url}")
+        tools.get(self.source_url)
+
+        self.output.info("Downloading prerequisites")
+        self.run(f"cd {self.gcc_folder} && ./contrib/download_prerequisites")
+
+    def build(self):
+        autotools = AutoToolsBuildEnvironment(self)
+
+        configure_args = [
+            "--disable-multilib", "--enable-plugin",
+            "--enable-languages=c,c++,lto",
+            # manually specify libexecdir, otherwise it points to bin/ and creates naming conflicts
+            "--libexecdir=${prefix}/libexec"
+        ]
+        if self.settings.arch_build == "armv7":
+            configure_args.append("--with-float=hard")
+
+        autotools.configure(configure_dir=os.path.join(self.source_folder, self.gcc_folder),
+                            args=configure_args)
+        autotools.make()
+
+    def package(self):
+        autotools = AutoToolsBuildEnvironment(self)
+        autotools.install()
+
+    def package_info(self):
+        # expose gcc
+        self.env_info.path.append(os.path.join(self.package_folder, "bin"))
+        self.env_info.CXX = os.path.join(self.package_folder, "bin", "g++")
+        self.env_info.CC = os.path.join(self.package_folder, "bin", "gcc")
+
+        # overwrite include path
+        self.cpp_info.includedirs = [os.path.join(self.package_folder, "include", "c++", self.version)]
